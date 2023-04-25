0b00304a2d4612c436a6a1e9ac08b535ecad1950
StoriFabio <fabinhostori@hotmail.com>
2023-04-25 20:31:31 -0300

    Aula 06

---
+++ 06-Quinta_Aula.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+echo "Introdução ao If e Else";
+?>
+
+<?php 
+$idade = 30;
+
+if($idade <18){
+    echo "Menor de Idade";
+}else{
+    echo "Maior de Idade!";
+}
+?>
+
+<?php 
+$x = 1;
+
+while($x <= 5){
+    echo "The number is: $x <br>";
+    $x++;
+}
+?>
+
+    
+</body>
+</html>
